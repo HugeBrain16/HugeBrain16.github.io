7bc5492548ad671d2df124065e6010a364203af0
HugeBrain16 <joshtuck373@gmail.com>
2021-10-19 09:12:42 +0200

    let's talk about about me, let's talk about the 6'8 frame the 37 in verticle leap...the black steel that drapes down my back aka the bullet proof mullet, the google prototype scopes with built in LCD LED 1080p 3D sony technology. The Ethiopian poisonous catapillar aka SLICK DADDY. lets talk about the cabinets right behind me that go 40ft deep that house the other 95% of my trophies, the awards, the certificates, all claiming first place, right? Let me give you a little inside glimpse into the hotshot, video game life style of the two time of the international video game superstar. because thats what the channels about, thats what this domain is about, that is what society is about.
    Signed-off-by: HugeBrain16 <joshtuck373@gmail.com>

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-var Ribbon = document.getElementById("Ribbon");
-var RibbonCtx = Ribbon.getContext("2d");
+$.getJSON("https://api.github.com/users/HugeBrain16/repos", (data) => {
+    $(".content-repo-count").html(data.length);
+});
 
-RibbonCtx.font = "80px Verdana";
-RibbonCtx.fillText("HugeBrain16", 20, 85);
-
-// get random HugeBrain16's repository
-var randomRepo = document.getElementById("randomRepo");
-var randomRepoURL = document.getElementById("randomRepoURL");
-
-$.getJSON("https://api.github.com/users/HugeBrain16/repos", function(data) {
-	let Repos = [];
-	const BASE_URL_IMG = "https://gh-card.dev/repos/HugeBrain16/"
-	const BASE_URL_GITHUB = "https://github.com/HugeBrain16/"
-
-	for (i = 0; i < data.length; i++) {
-		Repos.push(data[i]["name"]);
-	}
-
-	var repo = Repos[Math.floor(Math.random()*Repos.length)]
-	var image = BASE_URL_IMG + repo + ".svg";
-	var url = BASE_URL_GITHUB + repo;
-	randomRepo.src = image;
-	randomRepoURL.href = url;
+$(".content-jebait").click(function() {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // pepeLaugh
 });
